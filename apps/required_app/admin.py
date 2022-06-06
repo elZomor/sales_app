910021b9bd4ffae81bf951c95a3f982ee25bfad4
elzomor13 <52608235+elzomor13@users.noreply.github.com>
2022-06-06 02:26:08 +0200

    Add logging

--- apps/required_app/admin.py
+++ apps/required_app/admin.py
@@ -5,14 +5,20 @@ from apps.required_app.models import Engineer, Department, Client, Segment, SubS
 
 class ClientInline(admin.TabularInline):
     model = RequiredModel.client.through
+    verbose_name = "Client"
+    verbose_name_plural = "Clients"
 
 
 class SalesEngineerInline(admin.TabularInline):
     model = RequiredModel.sales_engineer.through
+    verbose_name = "Sales Engineer"
+    verbose_name_plural = "Sales Engineers"
 
 
 class StudyEngineerInline(admin.TabularInline):
     model = RequiredModel.study_engineer.through
+    verbose_name = "Study Engineer"
+    verbose_name_plural = "Study Engineers"
 
 
 class RequiredAdmin (admin.ModelAdmin):
